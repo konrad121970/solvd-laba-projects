4c8e181c216979cbcb51adaf93ffeefb778542fe
konrad121970 <konrad121970@windowslive.com>
2023-10-27 00:34:50 +0200

    Added selection sort algorithm

--- HW1/untitled/src/main/java/org/example/Main.java
+++ HW1/untitled/src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
 public class Main {
+
+    public static void selectionSort(int array[]){
+        for(int i = 0; i < array.length - 1; i++){
+            int minimal = i;
+            for(int j = i + 1; j < array.length; j++){
+                if(array[minimal] > array[j]){
+                    minimal = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[minimal];
+            array[minimal] = temp;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int array[] = {5, 6, 3, 2, 1, 7, 8, 9, 4};
+
+        System.out.println("### BEFORE SORTING ###");
+        for (int item :array) {
+            System.out.print(item + " ");
+        }
+
+        System.out.println();
+        System.out.println("### AFTER SORTING ###");
+        selectionSort(array);
+        for (int item :array) {
+            System.out.print(item + " ");
+        }
+
     }
 }
