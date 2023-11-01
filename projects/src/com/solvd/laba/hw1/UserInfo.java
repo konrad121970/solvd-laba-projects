bdc29525d873d5c6dfea975b90201e3db5731f03
konrad121970 <konrad121970@windowslive.com>
2023-11-01 23:44:59 +0100

    Change project structure

---
+++ projects/src/com/solvd/laba/hw1/UserInfo.java
@@ -0,0 +1,19 @@
+package com.solvd.laba.hw1;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("You have to specify three parameters: Name, Surname and Age.");
+            return;
+        }
+
+        String name = args[0];
+	    String surname = args[1];
+        int age = Integer.parseInt(args[2]);
+
+        System.out.println("User Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Age: " + age);
+    }
+}
