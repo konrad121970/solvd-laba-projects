e2eb374b3c08a568ff8dd429aa7165fedd6a9757
konrad121970 <konrad121970@windowslive.com>
2023-12-01 17:04:36 +0100

    Use stream in Vehicle util

--- src/main/java/com/solvd/laba/hw3/utils/VehicleUtils.java
+++ src/main/java/com/solvd/laba/hw3/utils/VehicleUtils.java
@@ -3,6 +3,7 @@ package com.solvd.laba.hw3.utils;
 import com.solvd.laba.hw3.model.vehicles.Vehicle;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class VehicleUtils {
 
@@ -15,11 +16,14 @@ public class VehicleUtils {
         vehicle.display();
 
         // Perform maintenance if needed
-        if (vehicle.getNextMaintenance() != null && LocalDate.now().isAfter(vehicle.getNextMaintenance())) {
-            vehicle.doMaintenance();
-            System.out.println("Maintenance performed for the vehicle.");
-        } else {
-            System.out.println("No maintenance needed for the vehicle.");
-        }
+        Optional.ofNullable(vehicle.getNextMaintenance())
+                .filter(nextMaintenanceDate -> LocalDate.now().isAfter(nextMaintenanceDate))
+                .ifPresentOrElse(
+                        nextMaintenanceDate -> {
+                            vehicle.doMaintenance();
+                            System.out.println("Maintenance performed for the vehicle.");
+                        },
+                        () -> System.out.println("No maintenance needed for the vehicle.")
+                );
     }
 }
